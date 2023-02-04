3c949abd16b692c929a2c95bb5d3d21448fb30f3
hayashiyoshino <twinklestaryoshino@gmail.com>
2023-02-04 16:26:37 +0900

    Initial Commit

---
+++ MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by Yoshino Hayashi on 2023/02/04.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
