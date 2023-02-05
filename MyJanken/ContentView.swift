32d90116bef674b44d90eea251c9f8aacde7601d
hayashiyoshino <twinklestaryoshino@gmail.com>
2023-02-05 10:58:10 +0900

    feature: 前回とは異なるじゃんけんの結果が表示されるよう変更

--- MyJanken/ContentView.swift
+++ MyJanken/ContentView.swift
@@ -42,7 +42,19 @@ struct ContentView: View {
             
             // [じゃんけんをする！]ボタン
             Button {
-                answerNumber = Int.random(in: 1...3)
+                // じゃんけんの結果を一時的に格納する新しい変数を設ける
+                var newAnswerNumber = 0
+                
+                // ランダムに結果を出すが、前回の結果と異なる場合のみ採用
+                repeat {
+                    newAnswerNumber = Int.random(in: 1...3)
+                    
+                    // 前回と同じ結果の時は再度ランダムに数値を出す
+                    // 異なる結果の時はrepeatを抜ける
+                } while answerNumber == newAnswerNumber
+                
+                // 新しいじゃんけんの結果を格納
+                answerNumber = newAnswerNumber
             } label: {
                 // Buttonに表示する文字を指定
                 Text("じゃんけんをする！")
